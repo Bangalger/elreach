6aa45a667ee77c9eacbe8bb85314e1329a961520
Juan Maria <juanmariamigliore@gmail.com>
2016-11-14 09:40:00 -0300

    Migrations

--- Migrations/20161026030248_CreateLevelsStationsTeams.php
+++ Migrations/20161026030248_CreateLevelsStationsTeams.php
@@ -2,7 +2,7 @@
 use Migrations\AbstractMigration;
 
 
-class CreateLevelsStations extends AbstractMigration
+class CreateLevelsStationsTeams extends AbstractMigration
 {
     /**
      * Change Method.
@@ -18,6 +18,8 @@ class CreateLevelsStations extends AbstractMigration
         ->addForeignKey('level_id','levels','id',array('delete'=>'CASCADE', 'update'=>'NO_ACTION'))
         ->addColumn('station_id', 'integer', array('signed'=> 'disable'))
         ->addForeignKey('station_id','stations','id',array('delete'=>'CASCADE', 'update'=>'NO_ACTION'))
+        ->addColumn('teams_id', 'integer')
+        ->addForeignKey('teams_id','teams','id',array('delete'=>'CASCADE', 'update'=>'NO_ACTION'))
         ->create();
     }
 }
